d4f3e603fae5c9058699e2b989704d947be05352
James Peng <jamespeng@Jamess-Air-10.pc.scharp.org>
2024-09-05 18:12:54 -0700

    updating threshold function

--- R/graphthresh.R
+++ R/graphthresh.R
@@ -8,7 +8,9 @@
 #' @return A \code{ggplot2} object displaying the estimates and confidence intervals for \eqn{E[E[Y|A \ge v, W]]} across the specified thresholds.
 #' @export
 graphthresh <- function(res,
-                        monotone = F){
+                        monotone = F,
+                        ylabel = "Estimate",
+                        xlabel = "Thresholds"){
 
   if (monotone){
     y_var <- "estimate_monotone"
@@ -16,9 +18,46 @@ graphthresh <- function(res,
   else{
     y_var <- "estimate"
   }
-  ggplot2::ggplot(res, aes(x = factor(threshold), y = !!sym(y_var))) +
+  ggplot2::ggplot(res, ggplot2::aes(x = threshold, y = !!sym(y_var))) +
     geom_point() +
-    geom_errorbar(aes(ymin = ci_lo, ymax = ci_hi), width = 0.2) +
+    geom_line() +
+    geom_ribbon(aes(ymin = ci_lo, ymax = ci_hi), alpha = 0.3, color = NA) +
     labs(x = "Thresholds", y = "Estimates (CI)") +
-    theme_minimal()
+    theme_minimal() +
+    scale_y_continuous(n.breaks = 10) +
+    scale_x_continuous(
+      limits = c(min(res$threshold), max(res$threshold))) +
+    xlab(xlabel) +
+    ylab(ylabel)
+
+}
+
+add_rcdf <- function(ggthresh,
+                     data,
+                     weights,
+                     marker){
+
+  scale_coef <- max(ggthresh$data$ci_hi, na.rm = T)
+
+  RCDF <- function(a) {
+    sum(data[[weights]] * (data[[marker]] >= a)) /
+      sum(data[[weights]]) * scale_coef
+  }
+
+  RCDF <- Vectorize(RCDF)
+
+  col <- c(col2rgb("olivedrab3")) # orange, darkgoldenrod2
+  col <- rgb(col[1], col[2], col[3], alpha = 255 * 0.4, maxColorValue = 255)
+
+  ggthresh +
+    stat_function(fun = RCDF, color = col,
+                  geom = "area", fill = col, alpha = 0.2) +
+    scale_y_continuous(
+      sec.axis = sec_axis(~ . / scale_coef,
+                          name = "Reverse CDF"), n.breaks = 10)  +
+    theme(plot.title = element_text(size = 25),
+          axis.text.x = element_text(angle = 0, hjust = 1, size = 18),
+          axis.text.y = element_text(angle = 0, hjust = 1, size = 18)) +
+    scale_x_continuous(limits = c(min(data[[marker]]), max(data[[marker]])))
+
 }
